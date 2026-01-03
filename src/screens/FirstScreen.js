1b669e8e24a2d10ee2a0026c1c0a53702faac9cd
daniel rozenblat <danielroz12345@gmail.com>
2026-01-04 00:46:21 +0200

    Newwwsדss

--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FirstScreen.module.css';
 import ContactForm from '../components/form/Form';
-
+import rokach from "../images/טאמבנייל.png"
 const HeroSection = () => {
-  return <>
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
     <div className={styles.heroContainer} dir="rtl">
       <div className={styles.contentWrapper}>
-        {/* אזור התוכן */}
         <div className={styles.contentSection}>
           <div className={styles.bgBlur}>
             <div className={styles.blurCircle1}></div>
@@ -14,26 +15,40 @@ const HeroSection = () => {
           </div>
           
           <div className={styles.textContent}>
-            {/* פסקת תיאור */}
             <p className={styles.descriptionB}>
               במיוחד לחיילים משוחררים
             </p>
-            
-            {/* כותרת ראשית */}
             <h1 className={styles.title}>איך למצוא כיוון קריירה ברור בלי להמר על תחום ובלי לפחד מהבחירה</h1>
           </div>
 
-          {/* סרטון Vimeo */}
           <div className={styles.videoContainer}>
             <div className={styles.videoPlayer}>
-              <iframe
-                className={styles.vimeoPlayer}
-                src="https://player.vimeo.com/video/1150842285?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
-                title="Course Preview Video"
-                frameBorder="0"
-                allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-                allowFullScreen
-              ></iframe>
+              {!isPlaying ? (
+                <div 
+                  className={styles.thumbnailWrapper}
+                  onClick={() => setIsPlaying(true)}
+                >
+                  <img 
+                    src={rokach}
+                    alt="צפה בסרטון"
+                    className={styles.thumbnail}
+                  />
+                  <button className={styles.playButton}>
+                    <svg viewBox="0 0 24 24" fill="white" width="48" height="48">
+                      <path d="M8 5v14l11-7z"/>
+                    </svg>
+                  </button>
+                </div>
+              ) : (
+                <iframe
+                  className={styles.vimeoPlayer}
+                  src="https://player.vimeo.com/video/1150842285?autoplay=1&title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
+                  title="Course Preview Video"
+                  frameBorder="0"
+                  allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+                  allowFullScreen
+                />
+              )}
             </div>
           </div>
 
@@ -41,10 +56,9 @@ const HeroSection = () => {
             <ContactForm />
           </div>
         </div>
-        
       </div>
     </div>
-  </>
+  );
 };
 
 export default HeroSection;
